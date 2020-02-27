727e30beab2d928e645c095d0c0b168e16abbb47
unknown <tyfanie.burnett@gmail.com>
2020-02-26 20:23:59 -0700

    this is the start of my hello world project

---
+++ Tyfanie.js
@@ -0,0 +1,22 @@
+var dan = "I lub you";
+var bob = "Hi My name is bob";
+bob = dan;
+var i = 0;
+
+
+
+while(i < 10){
+    console.log("I Won the game.")
+    i++
+}
+
+if(i == 0){
+    console.log("hi my name is kyler")
+}
+
+
+if(i < 5){
+    console.log(bob)
+}else{
+    console.log("my name is tyfanie")
+}
